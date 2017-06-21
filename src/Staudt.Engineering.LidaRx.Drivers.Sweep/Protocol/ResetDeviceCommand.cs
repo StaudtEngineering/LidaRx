77527239c57609448d6ba3bb3122fc311868d1b6
Yannic Staudt <yannic@staudt-engineering.com>
2017-06-21 15:57:57 +0200

    Implemented ResetDeviceCommand

---
+++ src/Staudt.Engineering.LidaRx.Drivers.Sweep/Protocol/ResetDeviceCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Staudt.Engineering.LidaRx.Drivers.Sweep.Protocol
+{
+    class ResetDeviceCommand : ISweepCommand
+    {
+        public char[] Command => new[] { 'R', 'R', '\n' };
+        public int ExpectedAnswerLength => 0;
+
+        public void ProcessResponse(char[] response)
+        {
+            // no response to RR
+        }
+    }
+}
+}

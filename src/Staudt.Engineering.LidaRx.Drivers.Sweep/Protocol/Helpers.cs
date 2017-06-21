9031883a2136ee0cde4a6eac0910ecb56c81eb31
Yannic Staudt <yannic@staudt-engineering.com>
2017-06-21 15:14:19 +0200

    Implemented helpers to decode sweep's ASCII encoded integers

---
+++ src/Staudt.Engineering.LidaRx.Drivers.Sweep/Protocol/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System;
+
+namespace Staudt.Engineering.LidaRx.Drivers.Sweep.Protocol
+{
+    class SweepProtocolHelpers
+    {
+        const int AsciiNumberBlockOffset = 48;
+
+
+        public static int AsciiBytesToInt(char[] input, int offset, int length)
+        {
+            var numbers = input.Skip(offset).Take(length).Select(x => x - AsciiNumberBlockOffset);
+
+            if (numbers.Any(x => x < 0 || x > 9))
+                throw new SweepProtocolError($"Failed to parse ASCII number at {offset} + {length} chars in this message", input);
+
+            return numbers.Reverse().Select((x, i) => x * (int)Math.Pow(10, i)).Sum();
+        }
+
+    }
+}

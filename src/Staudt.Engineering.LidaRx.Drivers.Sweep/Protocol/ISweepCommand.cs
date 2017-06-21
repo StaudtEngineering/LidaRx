48c78be472b83763a0cb82585953805d319aa078
Yannic Staudt <yannic@staudt-engineering.com>
2017-06-21 14:00:32 +0200

    Added MotorReadyCommand

---
+++ src/Staudt.Engineering.LidaRx.Drivers.Sweep/Protocol/ISweepCommand.cs
@@ -0,0 +1,11 @@
+﻿using RJCP.IO.Ports;
+
+namespace Staudt.Engineering.LidaRx.Drivers.Sweep.Protocol
+{
+    interface ISweepCommand
+    {
+        char[] Command { get; }
+        int ExpectedAnswerLength { get; }
+        void ProcessResponse(char[] reponse);
+    }
+}
---
+++ src/Staudt.Engineering.LidaRx.Drivers.Sweep/Protocol/MotorReadyCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Staudt.Engineering.LidaRx.Drivers.Sweep.Protocol
+{
+    class MotorReadyCommand : ISweepCommand
+    {
+
+        public char[] Command => new[] { 'M', 'Z', '\r' };
+        public int ExpectedAnswerLength => 5;
+
+
+        public void ProcessResponse(char[] response)
+        {
+            // check that the first two chars are 'M' and 'Z'
+            if (response[0] != Command[0] || response[1] != Command[1])
+                throw new SweepProtocolError("Expected answer to MZ command, received different header", response);
+
+            // decode the status
+            var r = new string(response, 2, 2);
+
+            this.DeviceReady = (r == "00");
+        }
+
+        #region results
+
+        public bool? DeviceReady { get; private set; } = null;
+
+        #endregion
+
+    }
+
+    
+}
